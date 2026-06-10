d91d9f462f6d6c961acb8385b3a94adc472e351a
hyungi99 <lee991229@naver.com>
2026-06-10 14:29:08 +0900

    Feat: 로그인/회원가입/소셜로그인 기능 추가

---
+++ components/domain/auth/login.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+/*
+  로그인 컴포넌트
+  모달에서 실행되며 이메일, 비밀번호로 로그인,
+  소셜 로그인을 포함하고 있다.
+*/
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { LoginData, LoginProps } from "@/types/auth.type";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { loginSchema } from "@/schemas/auth.schema";
+import SocialLogin from "./socialLogin";
+
+export default function Login({ onOpenChange, onSwitchToSignup }: LoginProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginData>({ resolver: zodResolver(loginSchema) });
+  const onSubmit = (data: LoginData) => console.log(data);
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col">
+          <div className="space-y-2">
+            <label htmlFor="email">이메일</label>
+            <Input
+              className="mt-1"
+              {...register("email")}
+              type="email"
+              placeholder="이메일"
+            />
+          </div>
+
+          {errors.email && (
+            <p className="text-red-500">{errors.email.message}</p>
+          )}
+
+          <div className="mt-4">
+            <label htmlFor="password">비밀번호</label>
+            <Input
+              className="mt-1"
+              {...register("password")}
+              type="password"
+              placeholder="비밀번호"
+            />
+          </div>
+        </div>
+
+        {errors.password && (
+          <p className="text-red-500">{errors.password.message}</p>
+        )}
+          <Button className="mt-2 bg-[#615ed6] h-9" type="submit">
+            로그인
+          </Button>
+          <div className="flex flex-col mt-6 gap-2">
+            <h2 className="font-bold text-xl">소셜 로그인</h2>
+            <SocialLogin onOpenChange={onOpenChange} />
+          </div>
+      </form>
+
+      <div className="flex mt-4 gap-1">
+        <p className="text-muted-foreground">아직 회원이 아니신가요? </p>
+        <span
+          onClick={onSwitchToSignup}
+          className="cursor-pointer hover:underline"
+        >
+          회원가입
+        </span>
+      </div>
+    </div>
+  );
+}
---
+++ components/domain/auth/socialLogin.tsx
@@ -0,0 +1,64 @@
+/*
+  소셜 로그인 버튼들로
+  카카오, 구글, 네이버로 이루어져 있다.
+*/
+
+import { SocialLoginProps } from "@/types/auth.type";
+import Image from "next/image";
+import kakaoLogin from "../../../public/auth/kakao_login.png";
+import googleLogin from "../../../public/auth/google_login.png";
+import naverLogin from "../../../public/auth/naver_login.png";
+
+export default function SocialLogin({ onOpenChange }: SocialLoginProps) {
+  const handleSocialLogin = (type: "kakao" | "google" | "naver") => {
+    onOpenChange(false);
+
+    const urls = {
+      kakao:
+        `https://kauth.kakao.com/oauth/authorize` +
+        `?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}` +
+        `&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}` +
+        `&response_type=code`,
+      google:
+        `https://accounts.google.com/o/oauth2/v2/auth` +
+        `?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}` +
+        `&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI}` +
+        `&response_type=code` +
+        `&scope=openid email profile`,
+      naver:
+        `https://nid.naver.com/oauth2.0/authorize` +
+        `?response_type=code` +
+        `&client_id=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}` +
+        `&redirect_uri=${process.env.NEXT_PUBLIC_NAVER_REDIRECT_URI}` +
+        `&state=naverLogin`,
+    };
+
+    window.location.replace(urls[type]);
+  };
+
+  return (
+    <div className="flex gap-2">
+      <button onClick={() => handleSocialLogin("kakao")}>
+        <Image
+          src={kakaoLogin}
+          className="cursor-pointer"
+          alt="카카오 로그인"
+          width={240}
+          height={80}
+        />
+      </button>
+      <button onClick={() => handleSocialLogin("google")}>
+        <Image src={googleLogin} className="cursor-pointer" alt="구글 로그인" width={240} height={80}/>
+      </button>
+      <button onClick={() => handleSocialLogin("naver")}>
+        <Image
+          src={naverLogin}
+          className="cursor-pointer"
+          alt="네이버 로그인"
+          width={240}
+          height={80}
+        />
+      </button>
+    </div>
+  );
+}

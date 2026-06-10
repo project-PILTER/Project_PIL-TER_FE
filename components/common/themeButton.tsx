ab1e9e7b26deb3e99a8192d7f64b13bbf4af097f
hyungi99 <lee991229@naver.com>
2026-06-10 14:28:40 +0900

    Feat: 다크/라이트 모드 바꿀 수 있는 버튼 추가

---
+++ components/common/themeButton.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+/*
+  테마버튼 컴포넌트
+  client쪽에서 렌더링되며 라이트모드/다크모드 바꾸기 가능
+*/
+
+import { useTheme } from "next-themes"
+import { Button } from "../ui/button";
+import { Moon, Sun } from "lucide-react";
+
+export default function ThemeButton() {
+  const {resolvedTheme, setTheme} = useTheme();
+  return(
+    <Button variant="ghost" onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}>{resolvedTheme === "dark" ? <Moon className="h-5 w-5" /> : <Sun className="h-5 w-5" />}</Button>
+  )
+}

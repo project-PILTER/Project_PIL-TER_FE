ca00a6969d5e477ca6700b649e5e6d68a9f4eca6
hyungi99 <lee991229@naver.com>
2026-06-11 18:13:00 +0900

    Design: 메인 페이지 디자인 변경

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,17 +4,26 @@
   하드코딩된 게시글이 1개 보인다.
 */
 
-import CommunityList from '@/components/domain/community/communityList';
+import { articles } from '@/components/domain/community/articleExamples';
 import '../globals.css';
+import ArticleCard from '@/components/domain/community/articleCard';
 import HeroSection from '@/components/domain/home/heroSection';
+import { TrendingUp } from 'lucide-react';
 
 export default function Home() {
+  const hotArticles = articles.filter((article) => article.isHot)
   // await new Promise((resolve) => setTimeout(resolve, 3000));
   return (
     <div>
       <HeroSection />
-      <div className='mx-auto max-w-7xl'>
-        <CommunityList />
+      <div className='mx-auto max-w-7xl flex items-center gap-2 mt-6'>
+        <TrendingUp className='' />
+        <h2 className='text-xl font-bold text-foreground'>인기 게시글</h2>
+      </div>
+      <div className='mx-auto max-w-7xl mt-2 grid grid-cols-1 md:grid-cols-2 gap-6'>
+        {hotArticles.map((article) => (
+          <ArticleCard key={article.id} article={article} />
+        ))}
       </div>
     </div>
   );

5b0a69de67a5cf2005a7ce6b1c957fd1dd3e32b5
hyungi99 <lee991229@naver.com>
2026-06-16 20:16:26 +0900

    Feat: 에디터 추가

--- components/common/editor/tiptap.tsx
+++ components/common/editor/tiptap.tsx
@@ -1,16 +1,12 @@
 "use client";
 
-import { EditorContent, useEditor } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
-import Placeholder from "@tiptap/extension-placeholder";
-import CharacterCount from "@tiptap/extension-character-count";
-import Image from "@tiptap/extension-image";
+import { EditorContent } from "@tiptap/react";
 import Toolbar from "./toolbar";
 import { useState } from "react";
+import { TiptapProps } from "@/types/editor.type";
 
-export default function Tiptap() {
+export default function Tiptap({editor, charactorCount}:TiptapProps) {
   const [preview, setPreview] = useState(false);
-  const [charactorCount, setCharactorCount] = useState(0);
 
   const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -21,26 +17,6 @@ export default function Tiptap() {
 
     editor.chain().focus().setImage({ src: imageUrl }).run();
   };
-
-  const editor = useEditor({
-    extensions: [
-      StarterKit,
-      Placeholder.configure({
-        placeholder: "질병 후기나 복용 경험을 작성해주세요.",
-      }),
-      CharacterCount.configure({
-        limit: 2000,
-      }),
-      Image,
-    ],
-    content: "",
-    onUpdate: ({ editor }) => {
-      setCharactorCount(editor.storage.characterCount.characters());
-    },
-    immediatelyRender: false,
-  });
-
-  if (!editor) return null;
 
   return (
     <div className="relative rounded-lg border-2 focus-within:border-violet-400 overflow:hidden">

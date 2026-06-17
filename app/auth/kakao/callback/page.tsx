b3848ad1f611a6cc92e7b52fbee32ed427798da0
hyungi99 <lee991229@naver.com>
2026-06-17 10:59:07 +0900

    Chore: 콜백 페이지 수정

--- app/auth/kakao/callback/page.tsx
+++ app/auth/kakao/callback/page.tsx
@@ -3,22 +3,18 @@
   소셜 로그인 콜백 페이지
   카카오 로그인 시 관련한 처리를 여기서 한다.
 */
-
-import { useRouter } from "next/router";
+import { useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
 export default function KakaoCallback() {
-  const router = useRouter();
+  const searchParmas = useSearchParams();
 
   useEffect(() => {
     const code = new URL(window.location.href).searchParams.get("code");
 
     console.log(code);
-
-    router.replace("/");
-  
     
-  }, [router]);
+  }, [searchParmas]);
 
   return (
     <div>카카오 로그인 로딩중...</div>
--- app/auth/naver/callback/page.tsx
+++ app/auth/naver/callback/page.tsx
@@ -1,21 +1,19 @@
 "use client";
-
 /*
   소셜 로그인 콜백 페이지
   네이버 로그인 시 관련 처리를 하는 페이지
 */
-
-import { useRouter } from "next/router";
+import { useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
 export default function NaverCallback() {
-  const router = useRouter();
+  const searchParams = useSearchParams();
 
   useEffect(() => {
     const code = new URL(window.location.href).searchParams.get("code");
 
     console.log(code);
-  }, [router]);
+  }, [searchParams]);
 
   return(
     <div>네이버 로그인 로딩중...</div>

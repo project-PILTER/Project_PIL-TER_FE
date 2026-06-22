cebf76f7e023d7d0e5493b0c31a5a9d7583adfc2
hyungi99 <lee991229@naver.com>
2026-06-22 10:22:11 +0900

    Chore: id 수정

--- components/domain/auth/userExamples.ts
+++ components/domain/auth/userExamples.ts
@@ -1,7 +1,7 @@
 import { CommentUser } from "@/types/community.type";
 
 export const commentCurrentUser:CommentUser = {
-  id: 1,
+  id: "user1",
   nickname: "lee99",
   profileImage: "/logo/logo.png"
 }

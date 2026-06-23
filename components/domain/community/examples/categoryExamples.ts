1c68b2b5569c6c0a8392d89363a03bb69cdfe3cf
hyungi99 <lee991229@naver.com>
2026-06-23 15:47:37 +0900

    Rename: Examples 폴더 이동

---
+++ components/domain/community/examples/categoryExamples.ts
@@ -0,0 +1,16 @@
+import { Category } from "@/types/community.type";
+
+export const categories: Category[] = [
+  {
+    id: 1,
+    name: "공통"
+  },
+  {
+    id: 2,
+    name: "암"
+  },
+  {
+    id: 3,
+    name: "정신건강"
+  }
+]

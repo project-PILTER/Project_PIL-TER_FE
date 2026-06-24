98467685385301e9b9e82ecc6c6694c12ebb5219
hyungi99 <lee991229@naver.com>
2026-06-24 18:30:47 +0900

    Feat: 건강일지 컴포넌트 추가

--- components/domain/community/journal/journalList.tsx
+++ components/domain/community/journal/journalList.tsx
@@ -1,5 +1,19 @@
-export default function JournalList() {
-  return(
-    <h1>journallist</h1>
-  )
+"use client";
+
+import { JournalDiary } from "@/types/journal.type";
+import JournalItem from "./journalItem";
+
+interface JournalListProps {
+  journals: JournalDiary[];
+  onEdit?: (journal: JournalDiary) => void;
+}
+
+export default function JournalList({ journals, onEdit }: JournalListProps) {
+  return (
+    <div className="space-y-4">
+      {journals.map((journal) => (
+        <JournalItem key={journal.id} journal={journal} onEdit={onEdit} />
+      ))}
+    </div>
+  );
 }

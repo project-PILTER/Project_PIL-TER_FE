e3c24589bfc98de21e791f96fe775c399ce8b6d2
hyungi99 <lee991229@naver.com>
2026-06-24 18:31:34 +0900

    Chore: 건강일지에 필요한 zod, 유틸 함수 추가

---
+++ schemas/journal.schema.ts
@@ -0,0 +1,19 @@
+import z from "zod";
+
+export const journalSchema = z.object({
+  journalDate: z.string(),
+
+  conditionStatus: z.enum([
+    "GOOD", "NORMAL", "BAD"
+  ]),
+
+  painScore: z.number().min(0, "통증 점수는 0 이상이어야 합니다.").max(10, "통증 점수는 10 이하여야 합니다."),
+
+  content: z.string().max(50, "내용은 50자 이하로 입력해주세요."),
+
+  symptoms: z.array(z.string()),
+
+  supplements: z.array(z.string())
+})
+
+export type JournalFormValues = z.infer<typeof journalSchema>;
---
+++ utils/journalStats.ts
@@ -0,0 +1,63 @@
+import { JournalDiary } from "@/types/journal.type";
+
+export function getJournalStats(journals: JournalDiary[]) {
+  const totalRecords = journals.length;
+  
+  const averagePain =
+    journals.length === 0
+      ? 0
+      : Number(
+          (
+            journals.reduce((sum, journal) => sum + journal.painScore, 0) /
+            journals.length
+          ).toFixed(1),
+        );
+
+  const conditionCount = {
+    GOOD: 0,
+    NORMAL: 0,
+    BAD: 0,
+  };
+
+  journals.forEach((journal) => {
+    conditionCount[journal.conditionStatus]++;
+  });
+
+  const conditionLabel = {
+    GOOD: "좋음",
+    NORMAL: "보통",
+    BAD: "나쁨",
+  };
+
+  const averageCondition =
+    conditionLabel[
+      Object.entries(conditionCount).sort(
+        (a, b) => b[1] - a[1],
+      )[0][0] as keyof typeof conditionLabel
+    ];
+
+  const sortedDates = journals
+    .map((journal) => journal.journalDate)
+    .sort((a, b) => new Date(b).getTime() - new Date(a).getTime());
+
+  let streak = 0;
+
+  for (let i = 0; i < sortedDates.length; i++) {
+    if (i === 0) {
+      streak++;
+      continue;
+    }
+
+    const current = new Date(sortedDates[i - 1]);
+    const next = new Date(sortedDates[i]);
+
+    const diff = (current.getTime() - next.getTime()) / (1000 * 60 * 60 * 24);
+
+    if (diff === 1) {
+      streak++;
+    } else {
+      break;
+    }
+  }
+  return { streak, averageCondition, averagePain, totalRecords };
+}

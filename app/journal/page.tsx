07ae6dddbd026ae5db3d13bad3f27a7583a58d1a
hyungi99 <lee991229@naver.com>
2026-06-29 19:51:02 +0900

    Feat: api 연동 추가

--- app/journal/page.tsx
+++ app/journal/page.tsx
@@ -1,7 +1,7 @@
 import { journalExamples } from "@/components/domain/community/examples/journalExamples";
 import JournalPageClient from "@/components/domain/community/journal/journalPageClint";
 
-export default function JournalPage() {
+export default async function JournalPage() {
   const journals = journalExamples;
   return(
     <JournalPageClient journals={journals} />

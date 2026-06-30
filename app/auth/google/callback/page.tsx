38e7fcc0d5218ce1555813d2a2e83b9e89959739
hyungi99 <lee991229@naver.com>
2026-06-30 15:42:22 +0900

    Fix: 예시 데이터에서 api 연동으로 변경

--- app/auth/google/callback/page.tsx
+++ app/auth/google/callback/page.tsx
@@ -1,18 +1,29 @@
 "use client";
+import { useAuthStore } from "@/stores/authStore";
 /*
   소셜 로그인 콜백 페이지
   구글 로그인을 했을 때 관련 처리를 여기서 한다.
 */
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
 export default function GoogleCallback() {
   const searchParmas = useSearchParams();
+  const router = useRouter();
+  const setUser = useAuthStore((state) => state.setUser);
 
   useEffect(() => {
     const code = new URL(window.location.href).searchParams.get("code");
 
-    console.log(code);
+    if(!code) {
+      alert("로그인 정보가 유효하지 않습니다. 다시로그인해주세요");
+      router.push("/");
+      return;
+    }
+
+    const processLogin = async() => {
+      
+    }
   
   }, [searchParmas])
 
--- app/community/articles/[id]/page.tsx
+++ app/community/articles/[id]/page.tsx
@@ -2,7 +2,7 @@ import ArticleDetail from "@/components/domain/community/article/articleDetail";
 import CommentList from "@/components/domain/community/comment/commentList";
 import { comments } from "@/components/domain/community/examples/commentExamples";
 import { getArticleDetail } from "@/services/community.service";
-import { notFound } from "next/navigation";
+import { redirect } from "next/navigation";
 
 interface ArticlePageProps {
   params: Promise<{id: string}> | {id: string}
@@ -12,21 +12,17 @@ export default async function ArticlePage({params}:ArticlePageProps) {
   const resolvedParams = await params;
   const articleId = Number(resolvedParams.id);
 
-  let article;
-  try {
-    article = await getArticleDetail(articleId);
-  } catch (error) {
-    notFound();
-  }
+  const article = await getArticleDetail(articleId);
 
   if(!article) {
-    notFound();
+    alert("검색된 게시글이 없습니다.");
+    redirect("/");
   }
 
   return (
     <div className="w-4xl mx-auto p-4 space-y-4">
       <div className="flex flex-col gap-4">
-        <ArticleDetail article={article} />
+        <ArticleDetail article={article} id={articleId}/>
       </div>
       <CommentList comments={comments} />
     </div>

d455941bd9e375becf4cd63885f4eafc3982381a
hyungi99 <lee991229@naver.com>
2026-06-30 15:44:29 +0900

    Feat: 건강일지 관련 api 호출 함수 추가

---
+++ services/journal.service.ts
@@ -0,0 +1,78 @@
+import {
+  JournalDiary,
+  JournalRequest,
+  OneJournalRequest,
+} from "@/types/journal.type";
+import { api } from "./axios";
+
+// 건강일지 조회
+export async function getJournals(): Promise<JournalDiary[] | null> {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/journals`, {
+      credentials: "include",
+    });
+
+    if (!res.ok) {
+      throw new Error(`건강일지 조회 실패 (Status: ${res.status})`);
+    }
+    return res.json();
+  } catch (error) {
+    console.error("건강일지 조회 실패", error);
+    return null;
+  }
+}
+
+// 건강일지 추가
+export async function postJournal(journalData: JournalRequest) {
+  try {
+    const res = await api.post("/journals", journalData);
+
+    return res.data;
+  } catch (error) {
+    console.error("건강일지 추가 실패", error);
+    throw error;
+  }
+}
+
+// 건강일지 단건 조회
+export async function getOneJournal(id: number) {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/journals/${id}`,
+      { credentials: "include" },
+    );
+
+    if (!res.ok) {
+      throw new Error(`건강일지 단건 조회 실패 (Status: ${res.status})`);
+    }
+
+    return res.json();
+  } catch (error) {
+    console.error("건강일지 단건 조회 실패", error);
+    throw error;
+  }
+}
+
+// 건강일지 수정
+export async function putJournal(journalData: OneJournalRequest, id: number) {
+  try {
+    const res = await api.put(`/journals/${id}`, journalData);
+
+    return res.data;
+  } catch (error) {
+    console.error("건강일지 수정 실패", error);
+    throw error;
+  }
+}
+
+// 건강일지 삭제
+export async function deleteJournal(id: number) {
+  try {
+    const res = await api.delete(`/journals/${id}`);
+
+    return res.data;
+  } catch (error) {
+    console.error("건강일지 삭제 실패", error);
+    throw error;
+  }
+}

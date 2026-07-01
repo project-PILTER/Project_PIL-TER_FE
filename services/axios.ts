4b50e794e2c488a151be47c9bf6ada07c0819e8c
hyungi99 <lee991229@naver.com>
2026-07-01 20:59:46 +0900

    Fix: url /api 추가

--- services/axios.ts
+++ services/axios.ts
@@ -5,7 +5,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
   timeout: 3000,
   withCredentials: true,
 });

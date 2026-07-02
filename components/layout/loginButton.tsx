8a17a68a3c382754ba76c92e95ee1fe82b80c6ca
hyungi99 <lee991229@naver.com>
2026-07-02 15:53:43 +0900

    Feat: dropdown menu 추가

--- components/layout/loginButton.tsx
+++ components/layout/loginButton.tsx
@@ -6,24 +6,59 @@
 */
 
 import { Button } from "../ui/button";
-import { LogIn } from "lucide-react";
+import { LogIn, LogOut, User } from "lucide-react";
 import { useState } from "react";
 import AuthModal from "../domain/auth/authModal";
 import Link from "next/link";
 import Image from "next/image";
 import { useAuthStore } from "@/stores/authStore";
+import { DropdownOption } from "@/types/ui.type";
+import { useRouter } from "next/navigation";
+import { logOut } from "@/services/auth.service";
+import Dropdown from "../common/dropdown";
 
 export default function LoginButton() {
+  const router = useRouter();
   const [open, setOpen] = useState(false);
   const [mode, setMode] = useState<"login" | "signup">("login");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   const user = useAuthStore((state) => state.user);
+
+  if(!user) {
+    router.push("/");
+    return null;
+  }
+  
+  const dropdownOptions: DropdownOption[] = [
+    {
+      label: "마이페이지",
+      icon: <User className="w-4 h-4" />,
+      onClick: () => router.push("/mypage"),
+      className: "hover:bg-gray-100 dark:hover:bg-neutral-100"
+    },
+    {
+      label: "로그아웃",
+      icon: <LogOut className="w-4 h-4" />,
+      onClick: async() => {
+        await logOut(user.id);
+        console.log("로그아웃 실행")
+      },
+      className: "hover:bg-red-50 dark:hover:bg-red-950"
+    }
+  ]
 
   if(user) {
     return(
-      <Link href="/mypage">
-        {user.profileImage ? (<Image src={user.profileImage} alt="프로필" width={40} height={40} className="rounded-full object-cover" />) : <div className="w-10 h-10 rounded-full bg-[#5c59da] text-white flex items-center justify-center">{user.nickname.charAt(0)}</div>}
-        
-      </Link>
+      <Dropdown options={dropdownOptions} align="end" trigger={
+        <Button>
+          {user.profileImage ? (
+            <Image src={user.profileImage} alt={user.nickname} width={40} height={40} className="rounded-full object-cover border border-gray-200 dark:border-neutral-100" />
+          ) : (
+            <div className="w-10 h-10 rounded-full text-white flex items-center justify-center font-medium">{user.nickname}</div>
+          )}
+        </Button>
+      } />
     )
   }
 

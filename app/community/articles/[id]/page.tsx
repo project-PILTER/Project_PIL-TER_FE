67187ebecea112602ed691b55837760088a9d8c2
hyungi99 <lee991229@naver.com>
2026-07-02 15:48:37 +0900

    Chore: 게시글 예시 데이터 추가

--- app/community/articles/[id]/page.tsx
+++ app/community/articles/[id]/page.tsx
@@ -1,9 +1,8 @@
 import ArticleDetail from "@/components/domain/community/article/articleDetail";
 import CommentList from "@/components/domain/community/comment/commentList";
 import { articles } from "@/components/domain/community/examples/articleExamples";
-import { comments } from "@/components/domain/community/examples/commentExamples";
 import { getArticleDetail } from "@/services/community.service";
-import { redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
 interface ArticlePageProps {
   params: Promise<{id: string}> | {id: string}
@@ -14,12 +13,16 @@ export default async function ArticlePage({params}:ArticlePageProps) {
   const articleId = Number(resolvedParams.id);
 
   // const article = await getArticleDetail(articleId);
+  // if(!article) {
+  //   redirect("/");
+  // }
+  
   const article = articles.find((item) => item.id === articleId);
-
   if(!article) {
-    alert("검색된 게시글이 없습니다.");
-    redirect("/");
+    notFound();
   }
+
+  const comments = article.comments;
 
   return (
     <div className="w-4xl mx-auto p-4 space-y-4">

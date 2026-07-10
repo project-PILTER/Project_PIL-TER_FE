a5d7ee732790666110b23bb87ae25862fe983b5a
hyungi99 <lee991229@naver.com>
2026-07-10 18:05:14 +0900

    Fix: symptoms, supplements 타입 변경

--- schemas/journal.schema.ts
+++ schemas/journal.schema.ts
@@ -11,9 +11,9 @@ export const journalSchema = z.object({
 
   content: z.string().max(50, "내용은 50자 이하로 입력해주세요."),
 
-  symptoms: z.array(z.string()),
+  symptoms: z.string(),
 
-  supplements: z.array(z.string())
+  supplements: z.string()
 })
 
 export type JournalFormValues = z.infer<typeof journalSchema>;

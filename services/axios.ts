05067909d1203dee4dbd56360672ef4133ec487b
hyungi99 <lee991229@naver.com>
2026-07-24 10:28:52 +0900

    Fix: timeout 10초로 증가

--- services/axios.ts
+++ services/axios.ts
@@ -7,7 +7,7 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
-  timeout: 5000,
+  timeout: 10000,
   withCredentials: true,
 });
 

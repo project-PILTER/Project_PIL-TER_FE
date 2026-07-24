331460e3c93005c00b09f0bd5d919706d585929f
hyungi99 <lee991229@naver.com>
2026-07-24 15:06:00 +0900

    Fix: 게시글 예시 데이터 삭제

--- app/community/articles/[id]/page.tsx
+++ app/community/articles/[id]/page.tsx
@@ -14,15 +14,16 @@ export default async function ArticlePage({params}:ArticlePageProps) {
   const resolvedParams = await params;
   const articleId = Number(resolvedParams.id);
 
-  // const article = await getArticleDetail(articleId);
-  // if(!article) {
-  //   redirect("/");
-  // }
-  
-  const article = articles.find((item) => item.id === articleId);
+  const article = await getArticleDetail(articleId);
+
   if(!article) {
-    notFound();
+    redirect("/");
   }
+
+  // const article = articles.find((item) => item.id === articleId);
+  // if(!article) {
+  //   notFound();
+  // }
 
   const comments = article.comments;
 

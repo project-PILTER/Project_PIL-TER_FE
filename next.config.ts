854b0cec68f89730ff8f889339b26d05dafad3d4
hyungi99 <lee991229@naver.com>
2026-07-24 15:04:54 +0900

    Feat: 구글, 네이버 사진 허용하도록 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**"
+      },
+      {
+        protocol: "https",
+        hostname: "ssl.pstatic.net",
+        port: "",
+        pathname: "/**"
+      }
+    ]
+  }
 };
 
 export default nextConfig;

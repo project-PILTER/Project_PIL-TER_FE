d969943e93694a914e7360a5429ec3909072e47a
lhg99 <lee991229@naver.com>
2026-08-24 16:10:52 +0900

    Chore: 맨 앞으로, 맨 뒤로 기능 추가

--- components/common/pagination.tsx
+++ components/common/pagination.tsx
@@ -1,4 +1,9 @@
-import { Pagination, PaginationContent, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "../ui/pagination";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+} from "../ui/pagination";
 
 interface PaginationProps {
   currentPage: number;
@@ -6,33 +11,50 @@ interface PaginationProps {
   basePath: string;
 }
 
-export default function CommonPagination({currentPage, totalPages, basePath}:PaginationProps) {
+export default function CommonPagination({
+  currentPage,
+  totalPages,
+  basePath,
+}: PaginationProps) {
   const pageGroupSize = 10;
-  const startPage = Math.floor((currentPage - 1) / pageGroupSize) * pageGroupSize + 1;
+  const startPage =
+    Math.floor((currentPage - 1) / pageGroupSize) * pageGroupSize + 1;
 
   const endPage = Math.min(startPage + pageGroupSize - 1, totalPages);
 
   const pages = Array.from(
-    {length: endPage - startPage + 1},
-    (_, i) => startPage + i
-  )
+    { length: endPage - startPage + 1 },
+    (_, i) => startPage + i,
+  );
 
   const previousPage = startPage - pageGroupSize;
-
   const nextPage = startPage + pageGroupSize;
 
-  return(
+  return (
     <Pagination>
       <PaginationContent>
+        {currentPage > 1 && (
+          <PaginationItem>
+            <PaginationLink href={`${basePath}?page=1`}>
+              &lt;&lt;
+            </PaginationLink>
+          </PaginationItem>
+        )}
+
         {startPage > 1 && (
           <PaginationItem>
-            <PaginationPrevious href={`${basePath}?page=${previousPage}`} />
+            <PaginationLink href={`${basePath}?page=${previousPage}`}>
+              &lt;
+            </PaginationLink>
           </PaginationItem>
         )}
 
         {pages.map((page) => (
           <PaginationItem key={page}>
-            <PaginationLink href={`${basePath}?page=${page}`} isActive={page === currentPage}>
+            <PaginationLink
+              href={`${basePath}?page=${page}`}
+              isActive={page === currentPage}
+            >
               {page}
             </PaginationLink>
           </PaginationItem>
@@ -40,10 +62,20 @@ export default function CommonPagination({currentPage, totalPages, basePath}:Pag
 
         {nextPage <= totalPages && (
           <PaginationItem>
-            <PaginationNext href={`${basePath}?page=${nextPage}`} />
+            <PaginationLink href={`${basePath}?page=${nextPage}`}>
+              &gt;
+            </PaginationLink>
+          </PaginationItem>
+        )}
+
+        {currentPage < totalPages && (
+          <PaginationItem>
+            <PaginationLink href={`${basePath}?page=${totalPages}`}>
+              &gt;&gt;
+            </PaginationLink>
           </PaginationItem>
         )}
       </PaginationContent>
     </Pagination>
-  )
+  );
 }

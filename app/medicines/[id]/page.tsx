29266e3b89e06bd33f8d5aad251827bb45edc3fc
lhg99 <lee991229@naver.com>
2026-08-24 16:10:39 +0900

    Feat: 약 후기 추가

--- app/medicines/[id]/page.tsx
+++ app/medicines/[id]/page.tsx
@@ -1,21 +1,30 @@
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 import MedicineHeader from "@/components/domain/medicine/medicineHeader";
 import MedicineInfo from "@/components/domain/medicine/medicineInfo";
+import MedicineReviews from "@/components/domain/medicine/review/medicineReviews";
 import { getMedicineDetail } from "@/services/medicine.client";
 
-export default async function MedicineDetailPage({params}: {params: Promise<{id: number}>}) {
+export default async function MedicineDetailPage({
+  params,
+}: {
+  params: Promise<{ id: number }>;
+}) {
   const { id } = await params;
 
   const medicineDetail = await getMedicineDetail(id);
 
   console.log("약 상세정보: ", medicineDetail);
-  return(
+  return (
     <div>
-      <MedicineHeader medicine={medicineDetail}/>
-      <div className="mx-auto max-w-7xl flex items-center gap-2 my-4">
+      <MedicineHeader medicine={medicineDetail} />
+      <div className="mx-auto max-w-7xl flex items-center gap-2 mt-4">
         <MedicineInfo medicine={medicineDetail} />
       </div>
+
+      <div className="mx-auto max-w-7xl flex items-center gap-2">
+        <MedicineReviews medicine={medicineDetail} />
+      </div>
     </div>
-  )
+  );
 }

84c6fdbf82716c419987a3a2ea1530699056d949
lhg99 <lee991229@naver.com>
2026-08-24 16:12:46 +0900

    Feat: 필요한 api 호출 함수 추가

--- services/medicine.client.ts
+++ services/medicine.client.ts
@@ -1,4 +1,4 @@
-import { MedicineResponse, Medicines } from "@/types/medicine.type";
+import { MedicineResponse, MedicineReviewRequest, Medicines } from "@/types/medicine.type";
 import { api } from "./axios";
 
 // 약 정보
@@ -21,6 +21,42 @@ export async function getMedicineDetail(id: number): Promise<Medicines> {
     return res.data;
   } catch (error) {
     console.error("약 상세정보 가져오기 실패", error);
+    throw error;
+  }
+}
+
+// 약 후기 작성
+export async function postMedicineReview(id: number, userId: number, data: MedicineReviewRequest) {
+  try {
+    const res = await api.post(`/medicines/${id}/reviews?userId=${userId}`, data);
+
+    return res.data;
+  } catch (error) {
+    console.error("약 후기 작성 실패", error);
+    throw error;
+  }
+}
+
+// 약 북마크
+export async function postMedicineBookmark(id: number, userId: number) {
+  try {
+    const res = await api.post(`/medicines/${id}/bookmark?userId=${userId}`);
+
+    return res.data;
+  } catch (error) {
+    console.error("약 북마크 실패", error);
+    throw error;
+  }
+}
+
+// 약 좋아요
+export async function postMedicineLike(id: number) {
+  try {
+    const res = await api.post(`/medicines/${id}/hot`);
+
+    return res.data;
+  } catch (error) {
+    console.error("약 좋아요 실패", error);
     throw error;
   }
 }
